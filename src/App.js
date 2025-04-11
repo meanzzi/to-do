cc744556e4e3a3d0c7fefe14898b6d7272fb28e2
meanzzi <meanzi.kim@gmail.com>
2025-04-11 19:51:28 +0900

    UI 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./component/Header";
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
